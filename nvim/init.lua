307daef00799cadb98c966555bec70d619bfcaf4
rase <rase@rase.com>
2025-01-11 16:34:14 +0200

    added highlight yank

--- nvim/init.lua
+++ nvim/init.lua
@@ -110,3 +110,12 @@ vim.keymap.set('n', '<leader>b', function()
     vim.cmd('!make build')  -- Run make build
 end, { noremap = true, desc = 'Run make build' })
 
+-- highlighting yank
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  desc = 'Hightlight selection on yank',
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
+  end,
+})
